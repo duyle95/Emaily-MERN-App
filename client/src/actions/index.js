2c60ddcc3839bb1f047d4bda14d2b77b3b791d7b
DuyLeNguyen <duy.lenguyen@edu.turkuamk.fi>
2017-10-21 15:46:37 +0300

    Final version

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 // when an action go through redux-thunk, if it see a function is returned, it calls that function right away
 // with dispatch function as param
@@ -11,4 +11,17 @@ export const fetchUser = () => async dispatch => {
 export const handleToken = (token) => async dispatch => {
   const res = await axios.post('/api/stripe', token);
   dispatch({ type: FETCH_USER, payload: res.data });
+}
+
+export const submitSurvey = (values, history) => async dispatch => {
+  const res = await axios.post('/api/surveys', values);
+
+  history.push('/surveys');
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get('/api/surveys');
+
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
 }
---
+++ sendgrid_webhook.js
@@ -0,0 +1,4 @@
+var localtunnel = require('localtunnel');
+localtunnel(5000, { subdomain: '8asd1fgf68hk4' }, function(err, tunnel) {
+  console.log('LT running')
+});
